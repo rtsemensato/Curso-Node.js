82e4e35d66501e3852d59829c0616dcd2975f8c9
Ricardo Semensato <rtestonsemensato@gmail.com>
2025-05-15 16:11:17 -0300

    tarefa 2

--- 2_FUNDAMENTOS/15_tarefa_2/index.mjs
+++ 2_FUNDAMENTOS/15_tarefa_2/index.mjs
@@ -4,18 +4,21 @@ import inquirer from "inquirer";
 inquirer
   .prompt([
     {
-      name: "Nome",
+      name: "nome",
       message: "Insira seu nome: ",
     },
     {
-      name: "Idade",
+      name: "idade",
       message: "Insira sua idade: ",
     },
   ])
   .then((answers) => {
+    if (!answers.nome) throw new Error("O nome é obrigatório");
+    if (!answers.idade) throw new Error("A idade é obrigatória");
+
     console.log(
       chalk.bgYellow.black.bold(
-        `O nome do usuário é ${answers["Nome"]}. Ele tem ${answers["Idade"]}`
+        `O nome do usuário é ${answers.nome} e sua idade é ${answers.idade} anos.`
       )
     );
   })
