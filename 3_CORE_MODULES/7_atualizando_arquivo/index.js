67f2a05cead1fc226bdecc1ac4fe1013f51a773b
Ricardo Semensato <rtestonsemensato@gmail.com>
2025-05-16 15:34:17 -0300

    mais core modules
    roteamento, atualizar, remover e renomear arquivos

---
+++ 3_CORE_MODULES/7_atualizando_arquivo/index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	const urlInfo = require('url').parse(req.url, true);
+
+	const name = urlInfo.query.name;
+
+	if (!name || name.trim() === '') {
+		fs.readFile('index.html', (err, data) => {
+			if (err) throw new Error(err);
+
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(data);
+			return res.end();
+		});
+	} else {
+		const nameNewLine = name + ',\r\n';
+
+		fs.appendFile('arquivo.txt', nameNewLine, (err, data) => {
+			if (err) throw new Error(err);
+
+			res.writeHead(302, { location: '/' });
+			return res.end();
+		});
+	}
+});
+
+server.listen(port, () => {
+	console.log(`Server rodando na porta ${port}...`);
+});
