a0b3c6d3e2aa02996716c4068ea027c5f4b9cb20
Ricardo Semensato <rtestonsemensato@gmail.com>
2025-05-28 17:35:46 -0300

    Dashboard e delete de pensamentos

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -9,10 +9,62 @@ module.exports = class toughtsController {
 	}
 
 	static async dashboard(req, res) {
-		res.render('toughts/dashboard');
+		const userId = req.session.userid;
+
+		const user = await User.findOne({ where: { id: userId }, include: Tought, plain: true });
+
+		//check if user exists
+		if (!user) {
+			res.redirect('/login');
+		}
+
+		const toughts = user.Toughts.map((result) => result.dataValues);
+
+		const emptyToughts = toughts.length === 0;
+
+		res.render('toughts/dashboard', {
+			toughts,
+			emptyToughts,
+		});
 	}
 
 	static createTought(req, res) {
 		res.render('toughts/create');
+	}
+
+	static async createToughtSave(req, res) {
+		const tought = {
+			title: req.body.title,
+			UserId: req.session.userid,
+		};
+
+		await Tought.create(tought);
+
+		req.flash('message', 'Pensamento criado com sucesso!');
+
+		try {
+			req.session.save(() => {
+				res.redirect('/toughts/dashboard');
+			});
+		} catch (error) {
+			console.log(`Aconteceu um erro: ${error}`);
+		}
+	}
+
+	static async removeTought(req, res) {
+		const id = req.body.id;
+		const userId = req.session.userid;
+
+		try {
+			await Tought.destroy({ where: { id: id }, UserId: userId });
+
+			req.flash('message', 'Pensamento criado com sucesso!');
+
+			req.session.save(() => {
+				res.redirect('/toughts/dashboard');
+			});
+		} catch (error) {
+			console.log(`Aconteceu um erro: ${error}`);
+		}
 	}
 };
--- 11_TOUGHTS/routes/toughtsRoutes.js
+++ 11_TOUGHTS/routes/toughtsRoutes.js
@@ -9,7 +9,9 @@ const router = express.Router();
 const checkAuth = require('../helpers/auth').checkAuth;
 
 router.get('/add', checkAuth, ToughtController.createTought);
+router.post('/add', checkAuth, ToughtController.createToughtSave);
 router.get('/dashboard', checkAuth, ToughtController.dashboard);
+router.post('/delete', checkAuth, ToughtController.removeTought);
 router.get('/', ToughtController.showToughts);
 
 module.exports = router;
