c628bb24d474f64182cff3ff6b45bde48ffbb3f1
Ricardo Semensato <rtestonsemensato@gmail.com>
2025-05-28 15:00:47 -0300

    Registro de nova conta e logout

---
+++ 11_TOUGHTS/controllers/AuthController.js
@@ -0,0 +1,66 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+module.exports = class AuthController {
+	static login(req, res) {
+		res.render('auth/login');
+	}
+
+	static register(req, res) {
+		res.render('auth/register');
+	}
+
+	static async registerPost(req, res) {
+		const { name, email, password, confirmpassword } = req.body;
+
+		//password match validation
+		if (password !== confirmpassword) {
+			req.flash('message', 'As senhas não conferem, tente novamente!');
+			res.render('auth/register');
+
+			return;
+		}
+
+		//check if user exists
+		const userExists = await User.findOne({ where: { email: email } });
+
+		if (userExists) {
+			req.flash('message', 'O e-mail já está em uso!');
+			res.render('auth/register');
+
+			return;
+		}
+
+		//create password
+		const salt = bcrypt.genSaltSync(10);
+		const hashedPassword = bcrypt.hashSync(password, salt);
+
+		const user = {
+			name,
+			email,
+			password: hashedPassword,
+		};
+
+		try {
+			const createdUser = await User.create(user, { raw: true });
+			console.log('createdUser.id: ', createdUser.id);
+			//initialize session
+			req.session.userid = createdUser.id;
+
+			req.flash('message', 'Cadastro realizado com sucesso!');
+
+			req.session.save(() => {
+				res.redirect('/');
+				// res.render('toughts/home');
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	static logout(req, res) {
+		req.session.destroy();
+
+		res.redirect('/login');
+	}
+};
