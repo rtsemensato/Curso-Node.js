e2ad6330ff41ad805ff7f358c83b7cf6e5791b7b
Ricardo Semensato <rtestonsemensato@gmail.com>
2025-05-30 14:17:01 -0300

    Módulo MongoDB completo. Criar, Read, Update, Delete

--- 12_MONGODB/controllers/ProductController.js
+++ 12_MONGODB/controllers/ProductController.js
@@ -1,7 +1,56 @@
-const Product = require('../models/Product');
+const Product = require('../models/Product.js');
 
 module.exports = class ProductController {
-	static showProducts(req, res) {
-		res.render('products/all');
+	static async showProducts(req, res) {
+		const products = await Product.getProducts();
+
+		res.render('products/all', { products });
+	}
+
+	static createProduct(req, res) {
+		res.render('products/create');
+	}
+
+	static async createProductPost(req, res) {
+		const product = new Product(req.body.name, req.body.image, req.body.price, req.body.description);
+
+		product.save();
+
+		res.redirect('/products');
+	}
+
+	static async getProduct(req, res) {
+		const id = req.params.id;
+
+		const product = await Product.getProductById(id);
+
+		res.render('products/product', { product });
+	}
+
+	static async removeProduct(req, res) {
+		const id = req.params.id;
+
+		await Product.removeProductById(id);
+
+		res.redirect('/products');
+	}
+
+	static async editProduct(req, res) {
+		const id = req.params.id;
+
+		const product = await Product.getProductById(id);
+		console.log('product: ', product);
+
+		res.render('products/edit', { product });
+	}
+
+	static async editProductPost(req, res) {
+		const id = req.body.id;
+
+		const product = new Product(req.body.name, req.body.image, req.body.price, req.body.description);
+
+		await product.updateProduct(id);
+
+		res.redirect('/products');
 	}
 };
--- 12_MONGODB/routes/productsRoutes.js
+++ 12_MONGODB/routes/productsRoutes.js
@@ -3,6 +3,12 @@ const ProductController = require('../controllers/ProductController');
 
 const router = express.Router();
 
+router.get('/create', ProductController.createProduct);
+router.post('/create', ProductController.createProductPost);
+router.post('/remove/:id', ProductController.removeProduct);
+router.get('/:id', ProductController.getProduct);
+router.get('/edit/:id', ProductController.editProduct);
+router.post('/edit', ProductController.editProductPost);
 router.get('/', ProductController.showProducts);
 
 module.exports = router;
