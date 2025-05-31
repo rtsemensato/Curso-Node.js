5426655f1c422e832210424cc6dc24b612854385
Ricardo Semensato <rtestonsemensato@gmail.com>
2025-05-31 19:33:50 -0300

    Criando as validações de insert de um novo usuário

---
+++ 15_GETAPET/backend/controllers/UserController.js
@@ -0,0 +1,46 @@
+const User = require('../models/User');
+
+module.exports = class UserController {
+	static async register(req, res) {
+		const { name, email, phone, password, confirmpassword } = req.body;
+
+		//validations
+		if (!name) {
+			res.status(422).json({ message: 'O nome é obrigatório' });
+			return;
+		}
+
+		if (!email) {
+			res.status(422).json({ message: 'O e-mail é obrigatório' });
+			return;
+		}
+
+		if (!phone) {
+			res.status(422).json({ message: 'O telefone é obrigatório' });
+			return;
+		}
+
+		if (!password) {
+			res.status(422).json({ message: 'A senha é obrigatória' });
+			return;
+		}
+
+		if (!confirmpassword) {
+			res.status(422).json({ message: 'A confirmação de senha é obrigatória' });
+			return;
+		}
+
+		if (password !== confirmpassword) {
+			res.status(422).json({ message: 'A senha e confirmação de senha devem ser iguais' });
+			return;
+		}
+
+		//check if user exists
+		const userExists = await User.findOne({ email: email });
+
+		if (userExists) {
+			res.status(422).json({ message: 'Já existe um usuário com esse e-mail. Por favor, utilize outro e-mail' });
+			return;
+		}
+	}
+};
