b096131d6b435af5d4a904aeb10146d2ee36b08b
Ricardo Semensato <rtestonsemensato@gmail.com>
2025-05-31 19:09:01 -0300

    Configuração inicial backend projeto pets

---
+++ 15_GETAPET/backend/models/Pet.js
@@ -0,0 +1,38 @@
+const mongoose = require('../db/conn');
+const { Schema } = mongoose;
+
+const Pet = mongoose.model(
+	'Pet',
+	new Schema(
+		{
+			name: {
+				type: String,
+				required: true,
+			},
+			age: {
+				type: Number,
+				require: true,
+			},
+			weight: {
+				type: Number,
+				required: true,
+			},
+			color: {
+				type: String,
+				required: true,
+			},
+			images: {
+				tpye: Array,
+				required: true,
+			},
+			available: {
+				type: Boolean,
+			},
+			user: Object,
+			adopter: Object,
+		},
+		{ timestamps: true }
+	)
+);
+
+module.exports = Pet;
